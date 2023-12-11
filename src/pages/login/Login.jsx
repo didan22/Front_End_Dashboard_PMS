465c80ba7d7fbfc6c688940684a0f442696702e5
Sofi Rahmatulloh <118430785+tikdivmu@users.noreply.github.com>
2023-12-11 10:37:41 +0700

    login menu

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,39 @@
+import React, { useState } from "react"
+import "./login.scss"
 
 export const Login = () => {
+
+  const [action, setAction] = useState("Login");
+
+
   return (
-    <div>Login</div>
+    <div className="container-login">
+      <div className="header-login">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        {action==="Login"?<div></div>:<div className="input">
+          <img src="/person.png" alt="" className="icon" />
+          <input type="text" placeholder="Name"/>
+        </div>}
+        
+        <div className="input">
+        <img src="/email.png" alt="" className="icon" />
+          <input type="email" placeholder="Email"/>
+        </div>
+        <div className="input">
+        <img src="/password.png" alt="" className="icon" />
+          <input type="password" placeholder="Password"/>
+        </div>
+      </div>
+      {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost Password ? <span>Click Here!</span></div>}
+      
+      <div className="submit-container">
+        <div className= {action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+        <div className= {action==="Sign Up"?"submit gray":"submit"}onClick={()=>{setAction("Login")}}>Login</div>
+      </div>
+    </div>
   )
 }
 
